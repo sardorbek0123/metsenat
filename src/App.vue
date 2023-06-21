235d3a3f8a9b8a69241fbeb4aaa64746828acc4a
Sardorbek <s.jabborov@uic.group>
2023-06-21 11:38:30 +0500

    starter

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <h1 class="text-3xl font-bold underline text-blue-400">
+      Hello world!
+    </h1>
+</template>
